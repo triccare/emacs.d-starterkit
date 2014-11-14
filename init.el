d1e99aced3c66288eaee98569f7ab2ac61f21957
Jonathan Eisenhamer <eisenham@hecatean.stsci.edu>
2014-11-14 15:40:44 -0500

    long overdue update.

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
