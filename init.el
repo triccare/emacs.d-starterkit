4ed76709ffcaedff9b87a5d6d4bc12638bf9be2e
triccare <triccare@gmail.com>
2015-03-04 18:22:44 -0500

    Added helm and helm-projectile

--- init.el
+++ init.el
@@ -4,6 +4,8 @@
 ;; Setup the starter kit.
 (require 'package)
 (add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 (when (not package-archive-contents)
@@ -22,7 +24,11 @@
                       yaml-mode
                       haml-mode
                       scss-mode
-                      jedi-direx)
+                      jedi-direx
+                      org
+                      helm
+                      helm-projectile
+                      )
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -35,6 +41,37 @@
 (setq-default backup-inhibited t)    ;Do not create ~ files.
 (setq-default indent-tabs-mode nil)  ;No tabs: required for HAML, and I just hate them anyways.
 (setq-default js-indent-level 2)     ;Javascript indent level.
+
+;; helm
+(require 'helm)
+(require 'helm-config)
+
+;; projectile
+(projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+(global-set-key (kbd "M-x") 'helm-M-x) ; command search
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
+(helm-mode 1)
 
 ;; Replace the undo system
 (global-undo-tree-mode)
